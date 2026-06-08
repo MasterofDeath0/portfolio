1cd0f7f08c92de3c4d9c3d29642b7786b606cf13
Sanyam Aggarwal <79480303+MasterofDeath0@users.noreply.github.com>
2026-06-09 02:12:20 +0530

    Update SpotifyNowPlaying.tsx

--- components/SpotifyNowPlaying.tsx
+++ components/SpotifyNowPlaying.tsx
@@ -64,7 +64,7 @@ export default function SpotifyNowPlaying() {
       >
         {track.isPlaying
           ? "Currently listening"
-          : "Recently listening to"}{" "}
+          : "Last played"}{" "}
         —
       </span>
 
